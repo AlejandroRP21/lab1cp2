884a31adc805075e1072387cb63f0f96f78444d2
AlejandroRP21 <alerivas032@gmail.com>
2025-03-20 11:31:38 -0600

    update

--- conexion.php
+++ conexion.php
@@ -1,11 +1,8 @@
 <?php
-
-    $conexion = mysqli_connect("localhost",
-    "root","",gorras sv"");
+    $conexion = mysqli_connect("localhost", "root", "", "gorrassv"); 
     if(mysqli_connect_errno()){
-        echo "Error: ".mysqli_connect_error();
-    }
-    else{
-        /*echo "Conexion realizada exitosamente";*/
+        echo "Error: " . mysqli_connect_error();
+    } else {
+        /* echo "Conexión realizada exitosamente"; */
     }
 ?>
